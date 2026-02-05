646be23b81631723aab6fa724c63daf217594b55
Swara Chokshi <swara.chokshi@growexx.com>
2026-02-05 15:41:11 +0530

    feat implement user logout Add discount calculation - Added the interactive
    add login

--- product.js
+++ product.js
@@ -5,8 +5,12 @@ this.price = price;
 this.discount = 0;
 }
 getPrice() {
+<<<<<<< HEAD
 const tax = 0.18; // 18% tax
 return this.price + (this.price * tax);
+=======
+return this.price - (this.price * this.discount / 100);
+>>>>>>> 31932c1 (Add discount calculation)
 }
 }
 module.exports = Product;
